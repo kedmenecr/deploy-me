c87169780ebf08f79abf82c8f8966bfd240fe7bc
Roberto Kedmenec <kedmenec.roberto@gmail.com>
2020-09-04 13:34:26 +0200

    reset

---
+++ build/precache-manifest.dd556acc6cbc7f379d89476218b396ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0de32124a86e8ca6951ca15fe142fad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd7d0ec84c9cd5c1a171",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5d9e264eb3af20632d52",
+    "url": "/static/js/2.4d1ab90d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4d1ab90d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd7d0ec84c9cd5c1a171",
+    "url": "/static/js/main.f4afdd9a.chunk.js"
+  },
+  {
+    "revision": "acd96e066c98744adfcc",
+    "url": "/static/js/runtime-main.0748300c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
